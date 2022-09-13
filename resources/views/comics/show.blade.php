6419136a753e1821127cd3855adf040219eb65cb
MyUsername <robertomazza99@gmail.com>
2022-09-13 16:01:06 +0200

    complete comics show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div class="text-center">
-        <h2>prova</h2>
-        <h3>{{ $comic->title }}</h3>
+    <div class="container text-center mt-3">
+        <h2>{{ $comic->title }}</h2>
+        <p class="p-3">{{ $comic->description }}</p>
+        <div>
+            <img src="{{ $comic->thumb }}" alt="">
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h4>Prezzo : {{ $comic->price }}</h4>
+                <h4>Serie : {{ $comic->series }}</h4>
+                <h4>Data di uscita : {{ $comic->sale_date }}</h4>
+                <h4>Categoria : {{ $comic->type }}</h4>
+            </div>
+        </div>
     </div>
 @endsection
