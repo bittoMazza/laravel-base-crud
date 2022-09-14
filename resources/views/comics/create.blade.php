6633b4f475f867498454388a79c1cc72560849a9
MyUsername <robertomazza99@gmail.com>
2022-09-14 14:23:15 +0200

    complete create.blade.php

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('main-content')
+<h2 class="text-center mt-2">Form per inserimento nuovo fumetto</h2>
+<div class="col-4 mx-auto p-3 mt-3 mb-2 bg-dark">
+    <form action="{{ route('comics.store') }}" method="POST">
+        <div class="form-group">
+          <label >Titolo</label>
+          <input type="text" class="form-control" name="title" placeholder="Inserire titolo fumetto" required>
+        </div>
+        <div class="form-group">
+            <label >Descrizione</label>
+            <textarea class="form-control" name="description" cols="30" rows="5"></textarea>
+        </div>
+        <div class="form-group">
+            <label >Copertina</label>
+            <input type="text" class="form-control" name="thumb" placeholder="Inserire Url copertina" required>
+        </div>
+        <div class="form-group">
+            <label >Prezzo</label>
+            <input type="text" class="form-control" name="price" placeholder="Inserire Prezzo" required>
+        </div>
+        <div class="form-group">
+            <label >Serie</label>
+            <input type="text" class="form-control" name="series" placeholder="Inserire serie del fumetto" required>
+        </div>
+        <div class="form-group">
+            <label >Data uscita</label>
+            <input type="date" class="form-control" name="sale_date" placeholder="Inserire data uscita fumetto" required>
+        </div>
+        <div class="form-group">
+            <label >Categoria</label>
+            <input type="text" class="form-control" name="type" placeholder="Inserire categoria fumetto" required>
+        </div>
+        <button type="submit" class="btn btn-primary my-3">Inserisci nuovo fumetto</button>
+      </form>
+</div>
+@endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,6 +3,7 @@
         <ul>
             <li><a class="fs-4" href="{{ route('admin.home') }}">Admin Home</a></li>
             <li><a class="fs-4" href="{{ route('comics.index') }}">Index Comics</a></li>
+            <li><a class="fs-4" href="{{ route('comics.create') }}">Add comics</a></li>
         </ul>
     </nav>
 </header>
